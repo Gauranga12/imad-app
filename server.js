f530ba82fbf43e36005b9cd76f27bb71cdcba408
Gauranga12 <30649909+Gauranga12@users.noreply.github.com>
2017-09-04 17:05:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var articles={article-one:
-    
-};
+
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/article-one', function (req, res) {
+  res.send('article one requested will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
