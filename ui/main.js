06b6e0143bd68e9b297f090e2f0e0bd8dd5f3952
Gauranga12 <30649909+Gauranga12@users.noreply.github.com>
2017-11-10 15:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,28 @@ var btn=document.getElementById('btn');
 var counter=0;
 btn.onclick = function(){
     counter=counter + 1;
-    var span=document.getElementById('count');
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://wwwnarasimhashenoy007.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
+submit.onclick = function(){
+    var name=document.getElementById('name');
+    var id=document.getElementById('id');
+    var dept=document.getElementById('dept');
+
     span.innerHTML=counter.toString();
+   
 };
